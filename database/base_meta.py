c919ef650f660ecaeae15d0cee79f9fddebcee1d
Jorge <you@example.com>
2023-05-08 14:46:54 +0500

    add config.ini and port in global_init()

--- database/base_meta.py
+++ database/base_meta.py
@@ -7,7 +7,7 @@ Base = dec.declarative_base()
 __factory = None
 
 
-def global_init(user, password, host, db_name):
+def global_init(host, port, user, password, db_name):
     global __factory
 
     if __factory:
@@ -16,7 +16,7 @@ def global_init(user, password, host, db_name):
     if not db_name:
         raise AttributeError("Не задано имя файлы файла базы данных")
 
-    connection_string = f'postgresql://{user}:{password}@{host}:5432/{db_name}'
+    connection_string = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
     print(f"Подключение к БД: {connection_string}")
 
     engine = sa.create_engine(connection_string, echo=True)
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from database import global_init, create_session, Hero, Item
 from parse.parse import parsing
+import configparser
 
-global_init("postgres", "utezam", "localhost", "dota")
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+
+database_host = config['database']['host']
+database_port = config['database']['port']
+database_username = config['database']['username']
+database_password = config['database']['password']
+database_name = config['database']['name']
+
+global_init(database_host, database_port, database_username, database_password, database_name)
 session = create_session()
 
 items = session.query(Item).all()
@@ -9,4 +19,4 @@ items = session.query(Item).all()
 for parent in items:
     print(parent)
 
-parsing("https://liquipedia.net/dota2/Item_statistics")
+# parsing("https://liquipedia.net/dota2/Item_statistics")
